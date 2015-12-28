2c1cc30940244d2243a080c316e10dd2a8e51888
jason-yang31415 <jason.yang31415@gmail.com>
2015-12-27 21:14:37 -0500

    Fixed copyright notice in Matrix4f

--- src/util/Matrix4f.java
+++ src/util/Matrix4f.java
@@ -1,14 +1,25 @@
-
 /*
  * The MIT License (MIT)
-
-import java.nio.FloatBuffer;
-import org.lwjgl.BufferUtils;
-
-/**
- * This class represents a 4x4-Matrix. GLSL equivalent to mat4.
  *
- * @author Heiko Brumme
+ * Copyright © 2015, Heiko Brumme
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package util;
 
@@ -16,6 +27,11 @@ import java.nio.FloatBuffer;
 
 import org.lwjgl.BufferUtils;
 
+/**
+ * This class represents a 4x4-Matrix. GLSL equivalent to mat4.
+ *
+ * @author Heiko Brumme
+ */
 public class Matrix4f {
 
     public float m00, m01, m02, m03;
