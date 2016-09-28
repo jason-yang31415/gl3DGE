41921021885354cd17948b15a0a01bda1d2be891
Jason Yang <jason.yang31415@gmail.com>
2016-09-27 20:27:40 -0400

    Moved text shader and VDO to TTF class
    allows for immediate text generation and drawing

--- src/render/text/Text.java
+++ src/render/text/Text.java
@@ -2,40 +2,34 @@ package render.text;
 
 import static org.lwjgl.stb.STBTruetype.stbtt_GetBakedQuad;
 import static org.lwjgl.system.MemoryStack.stackPush;
-import static org.lwjgl.glfw.GLFW.glfwGetTime;
 
-import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import logic.Transform;
+
 import org.lwjgl.stb.STBTTAlignedQuad;
 import org.lwjgl.system.MemoryStack;
 
 import render.Drawable;
-import render.VertexDataObject;
 import render.mesh.Mesh;
 import render.mesh.Vertex;
-import render.shader.nodes.AlphaTestSN;
-import render.shader.nodes.NodeBasedShader;
-import render.shader.nodes.SamplerSN;
-import render.shader.nodes.SamplerSNV;
-import render.shader.nodes.ShaderNodeValue;
 import util.Matrix4f;
 import util.Vector2f;
 import util.Vector3f;
 
-public class Text {
+public class Text extends Transform {
 
 	private String text;
 	private int lineCount;
 	
 	TrueTypeFont ttf;
 	
-	NodeBasedShader ts;
-	VertexDataObject vdo;
+	//NodeBasedShader ts;
+	//VertexDataObject vdo;
 	
 	public Text(String text, TrueTypeFont ttf){
 		this.ttf = ttf;
@@ -47,51 +41,10 @@ public class Text {
 			lc++;
 		
 		lineCount = lc;
-		
-		loadShader();
 	}
 	
 	public String getText(){
 		return text;
-	}
-	
-	public void loadShader(){
-		ts = new NodeBasedShader();
-		ts.addInput(ShaderNodeValue.INPUT_POSITION, ts.getInputNode()
-				.getOutPosition());
-		ts.addInput(ShaderNodeValue.INPUT_TEXTURE_COORDINATE, ts
-				.getInputNode().getOutTextureCoordinate());
-		
-		ts.addSampler(ttf.getTexture());
-		
-		SamplerSN texSampler2 = new SamplerSN(ts);
-		SamplerSNV texSNV2 = new SamplerSNV(null, "texture");
-		texSNV2.setSampler(ttf.getTexture());
-		ts.addUniform("texture", texSNV2);
-		texSampler2.setInSampler(texSNV2);
-		texSampler2.setInTextureCoordinate(ts.getInputNode()
-				.getOutTextureCoordinate());
-		ts.addNode(texSampler2);
-		
-		AlphaTestSN at = new AlphaTestSN(ts);
-		at.setInValue(texSampler2.getOutColor4f());
-		ts.addNode(at);
-		
-		/*ValueSNV color = new ValueSNV(null, "color");
-		color.defineAsVector3f(new Vector3f(1, 1, 1));
-		ts.addConstant(color);*/
-		ts.getOutputNode().setInValue(texSampler2.getOutColor4f());
-		
-		try {
-			ts.loadShaders();
-			ts.check();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ts.init();
-		vdo = new VertexDataObject();
-		vdo.loadVAO();
 	}
 	
 	public void draw(){
@@ -104,13 +57,13 @@ public class Text {
 			for ( int i = 0; i < text.length(); i++ ) {
 				char c = text.charAt(i);
 				if ( c == '\n' ) {
-					y.put(0, y.get(0) + 24); //24 = font height
+					y.put(0, y.get(0) + ttf.getFontSize());
 					x.put(0, 0.0f);
 					continue;
 				} else if ( c < 32 || 128 <= c )
 					continue;
 				
-				stbtt_GetBakedQuad(ttf.getCData(), ttf.getBitmapWidth(), ttf.getBitmapHeight(), c - 32, x, y, q, true);
+				stbtt_GetBakedQuad(ttf.getCData(), ttf.getBitmapSize(), ttf.getBitmapSize(), c - 32, x, y, q, true);
 				
 				Mesh mesh = new Mesh();
 				ArrayList<Vertex> verts2 = new ArrayList<Vertex>();
@@ -129,17 +82,18 @@ public class Text {
 				ArrayList<Integer> indices2 = new ArrayList(Arrays.asList(index_array2));
 				mesh.loadIndices(indices2);
 				
-				if (vdo.getVBO() != null)
-					vdo.getVBO().delete();
-				vdo.loadVBO(mesh, ts);
-				if (vdo.getEBO() != null)
-					vdo.getEBO().delete();
-				vdo.loadEBO(mesh);
+				if (ttf.getVDO().getVBO() != null)
+					ttf.getVDO().getVBO().delete();
+				ttf.getVDO().loadVBO(mesh, ttf.getShader());
+				if (ttf.getVDO().getEBO() != null)
+					ttf.getVDO().getEBO().delete();
+				ttf.getVDO().loadEBO(mesh);
 				
 				float ratio = 1;
-				Matrix4f model = Matrix4f.scale(1 / 960f, 1 / 540f, 1).multiply(Matrix4f.translate(-0.5f, -0.5f, 0));
-				Drawable t = new Drawable(ts, vdo);
-				ts.setMVP(model, new Matrix4f(), new Matrix4f());
+				//Matrix4f model = new Matrix4f();
+				Matrix4f projection = Matrix4f.orthographic(0, 960, 0, 540, -1, 1);
+				Drawable t = new Drawable(ttf.getShader(), ttf.getVDO());
+				ttf.getShader().setMVP(getMatrix(), new Matrix4f(), projection);
 				t.draw();
 			}
 		}
--- src/render/text/TrueTypeFont.java
+++ src/render/text/TrueTypeFont.java
@@ -3,7 +3,6 @@ package render.text;
 import static org.lwjgl.opengl.GL11.GL_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
 import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
-import static org.lwjgl.opengl.GL11.glGenTextures;
 import static org.lwjgl.stb.STBTruetype.stbtt_BakeFontBitmap;
 import io.IOUtil;
 
@@ -16,44 +15,102 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.stb.STBTTBakedChar;
 
 import render.SamplerMap;
+import render.VertexDataObject;
+import render.shader.nodes.AlphaTestSN;
+import render.shader.nodes.NodeBasedShader;
+import render.shader.nodes.SamplerSN;
+import render.shader.nodes.SamplerSNV;
+import render.shader.nodes.ShaderNodeValue;
 
 public class TrueTypeFont {
 	
-	private int BITMAP_W = 512;
-	private int BITMAP_H = 512;
+	private int BITMAP_SIZE = 512;
 	
 	private STBTTBakedChar.Buffer cdata;
 	
 	private SamplerMap tex;
+	private int fontSize;
+	
+	private NodeBasedShader ts;
+	private VertexDataObject vdo;
 
-	public TrueTypeFont(String path){
-		int id = glGenTextures();
-		cdata = STBTTBakedChar.malloc(96);
+	public TrueTypeFont(String path, int fontSize){
+		this.fontSize = fontSize;
 		
+		cdata = STBTTBakedChar.malloc(96);
 		try {
 			ByteBuffer ttf = IOUtil.ioResourceToByteBuffer(path, 160 * 1024);
 			
-			ByteBuffer bitmap = BufferUtils.createByteBuffer(BITMAP_W * BITMAP_H);
-			stbtt_BakeFontBitmap(ttf, 24, bitmap, BITMAP_W, BITMAP_H, 32, cdata); //change values, 24 = font height
+			ByteBuffer bitmap = BufferUtils.createByteBuffer(BITMAP_SIZE * BITMAP_SIZE);
+			stbtt_BakeFontBitmap(ttf, fontSize, bitmap, BITMAP_SIZE, BITMAP_SIZE, 32, cdata);
 			
-			tex = new SamplerMap(BITMAP_W, BITMAP_H, SamplerMap.TEX_DEFAULT);
-			tex.texImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, BITMAP_W, BITMAP_H, 0, GL_ALPHA, GL_UNSIGNED_BYTE, bitmap);
+			tex = new SamplerMap(BITMAP_SIZE, BITMAP_SIZE, SamplerMap.TEX_DEFAULT);
+			tex.texImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, BITMAP_SIZE, BITMAP_SIZE, 0, GL_ALPHA, GL_UNSIGNED_BYTE, bitmap);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		loadShader();
+	}
+	
+	public void loadShader(){
+		ts = new NodeBasedShader();
+		ts.addInput(ShaderNodeValue.INPUT_POSITION, ts.getInputNode()
+				.getOutPosition());
+		ts.addInput(ShaderNodeValue.INPUT_TEXTURE_COORDINATE, ts
+				.getInputNode().getOutTextureCoordinate());
+		
+		ts.addSampler(getTexture());
+		
+		SamplerSN texSampler2 = new SamplerSN(ts);
+		SamplerSNV texSNV2 = new SamplerSNV(null, "texture");
+		texSNV2.setSampler(getTexture());
+		ts.addUniform("texture", texSNV2);
+		texSampler2.setInSampler(texSNV2);
+		texSampler2.setInTextureCoordinate(ts.getInputNode()
+				.getOutTextureCoordinate());
+		ts.addNode(texSampler2);
+		
+		AlphaTestSN at = new AlphaTestSN(ts);
+		at.setInValue(texSampler2.getOutColor4f());
+		ts.addNode(at);
+		
+		/*ValueSNV color = new ValueSNV(null, "color");
+		color.defineAsVector3f(new Vector3f(1, 1, 1));
+		ts.addConstant(color);*/
+		ts.getOutputNode().setInValue(texSampler2.getOutColor4f());
+		
+		try {
+			ts.loadShaders();
+			ts.check();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ts.init();
+		vdo = new VertexDataObject();
+		vdo.loadVAO();
+	}
+	
+	public VertexDataObject getVDO(){
+		return vdo;
+	}
+	
+	public NodeBasedShader getShader(){
+		return ts;
 	}
 	
 	public STBTTBakedChar.Buffer getCData(){
 		return cdata;
 	}
 	
-	public int getBitmapWidth(){
-		return BITMAP_W;
+	public int getBitmapSize(){
+		return BITMAP_SIZE;
 	}
 	
-	public int getBitmapHeight(){
-		return BITMAP_H;
+	public int getFontSize(){
+		return fontSize;
 	}
 	
 	public SamplerMap getTexture(){
